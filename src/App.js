edd72430c6513b676ad9a9b245c2ec679382d2a5
t1ger <jianbo.wu@shopee.com>
2018-07-06 11:06:24 +0800

    add index data-fetching

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { h } from '../hyperapp-impl'
 import Index from './components/Index'
 
 export default (state, actions) => {
-
   return (
     <div>
       <Index state={state} actions={actions} />
